0b611fc101c42ce8f695af600e13ab2919aee554
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-04-16 09:53:51 -0400

    Add actions and client-fetchers

--- lib/keys.ts
+++ lib/keys.ts
@@ -1,13 +1,11 @@
 export type FetchStoriesKey = { type: string, league?: string, noUser: boolean, page: number, noLoad: boolean, firstXid?: string };
-export type FetchMyFeedKey = { type: string, league?: string, noUser: boolean, page: number, teamid: string, name: string, myteam: number, noLoad: boolean };
-  
+export type FetchMyFeedKey = { type: string, league?: string, noUser: boolean, page: number, teamid: string, name: string, myteam: number, noLoad: boolean };  
 export type ASlugStoryKey = { type: string, slug: string, noLoad: boolean };
 export type AMentionKey = { type: string, findexarxid: string, noLoad: boolean };
 export type FavoritesKey = { type: string, league?: string, noUser: boolean, page: number };
-
 export type TeamPlayersKey = { type: string, league: string; teamid: string };
 export type MyTeamRosterKey = { type: string, league: string, noUser: boolean, noLoad: boolean };
-
 export type PlayerPhotoKey = { func: string, name: string, teamid: string };
 export type MetaLinkKey = { func: string, findexarxid: string, long?: number };
-   
+export type LeagueTeamsKey = { func: string, league: string, noLoad: boolean };
+     
