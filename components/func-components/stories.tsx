7eea2a1e0a659c671b22bbd1b880ea2b001cd4d0
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-05-19 15:19:10 -0400

    Revert to revalidateAll to test eliminiating duplicates in the feed.

--- components/func-components/stories.tsx
+++ components/func-components/stories.tsx
@@ -71,7 +71,7 @@ const Stories: React.FC<Props> = () => {
         if (previousPageData && !previousPageData.length) return null; // reached the end
         return key;
     };
-    const { data, mutate, size, setSize, isLoading } = useSWRInfinite(fetchStoriesKey, actionStories, { initialSize: 1, revalidateAll: true, parallel: true, fallback, revalidateFirstPage: true });
+    const { data, mutate, size, setSize, isLoading } = useSWRInfinite(fetchStoriesKey, actionStories, { initialSize: 1, revalidateAll: true, parallel: true, fallback, /*revalidateFirstPage: true*/ });
 
     let stories = data ? [].concat(...data) : [];
 
