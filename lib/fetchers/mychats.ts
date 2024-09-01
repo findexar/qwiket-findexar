7da7a2b7e725e2568de9c4ce46102d9a3da859fa
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-09-01 10:13:00 -0400

    Add MyChats fetcher

--- lib/fetchers/mychats.ts
+++ lib/fetchers/mychats.ts
@@ -2,46 +2,45 @@
 import { unstable_serialize } from 'swr'
 import { MyChatsKey } from '@/lib/keys';
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { getIronSession } from "iron-session";
+import { sessionOptions, SessionData } from "@/lib/session";
 import { Chat } from "@/lib/types/chat";
+import { cookies } from "next/headers";
 const api_key = process.env.LAKE_API_KEY;
-export const fetchMyChats = async (key: MyChatsKey, userId: string): Promise<{ subscrLevel: number }> => {
+export const fetchMyChats = async (key: MyChatsKey, userId: string, sessionid: string, page: number): Promise<Chat[]> => {
     try {
-        const {league,teamid,athleteUUId}=key;
+        const { league = '', teamid = '', athleteUUId = '' } = key;
         let url = '';
 
-        url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v50/findexar/ai-chat/fetch-chats?api_key=${api_key}&userid=${userId || ""}&league=${league}&sessionid=${sessionid}&page=${page}`;
+        url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v50/findexar/ai-chat/fetch-chats?api_key=${api_key}&userid=${userId || ""}&league=${league}&teamid=${teamid}&athleteUUId=${athleteUUId}&sessionid=${sessionid}&page=${page}`;
+        console.log("fetching my chats", url);
         const fetchResponse = await fetch(url);
         const data = await fetchResponse.json();
-        console.log("fetching user subscription", url, data);
+        console.log("return fetching my chats", url, data);
         if (data.success) {
-            console.log("===>GET USER SUBSCRIPTION", data.subscription);
-            return data.subscription as { subscrLevel: number };
+            console.log("===>GET MY CHATS", data.chats);
+            return data.chats as Chat[];
         }
-        throw new Error("Failed to get user subscription");
+        throw new Error("Failed to fetchMyChats");
     }
     catch (e) {
-        console.log("getAMention", e);
-        throw new Error("Failed to get user subscription");
+        console.log("fetchMyChats", e);
+        throw new Error("Failed to fetchMyChats");
     }
 }
 
-const promiseUserSubscription = async (key: UserSubscriptionKey, userId: string, email: string) => {
-    return { key: unstable_serialize(key), call: getUserSubscription(userId, email) };
+const promiseMyChats = async (key: MyChatsKey, userId: string, sessionid: string) => {
+    return { key: unstable_serialize(key), call: fetchMyChats(key, userId, sessionid, 0) };
 }
-export const actionUserSubscription = async (key: UserSubscriptionKey): Promise<{ subscrLevel: number }> => {
+export const actionMyChats = async (key: MyChatsKey): Promise<Chat[]> => {
     'use server';
-    let { userId } = auth();
+    const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+    const { userId } = auth() || { userId: "" };
+    const sessionid = session.sessionid;
 
     if (!userId) {
-        userId = "";
+        return [];
     }
-    if (!userId) {
-        return { subscrLevel: 0 };
-    }
-
-    const user = await currentUser();
-    const email = user?.emailAddresses[0]?.emailAddress || "";
-
-    return getUserSubscription(userId, email);
+    return fetchMyChats(key, userId, sessionid, 0);
 }
-export default promiseUserSubscription;
+export default promiseMyChats;
