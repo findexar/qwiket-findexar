7add7f7e94c01424c68f7ea86bf088cc4e8507fa
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-12-06 13:37:33 -0500

    Add userid to record-event

--- lib/server-actions/event.ts
+++ lib/server-actions/event.ts
@@ -3,29 +3,29 @@ import { MyTeamKey } from "@/lib/keys";
 import { unstable_serialize } from 'swr'
 import { cookies } from "next/headers";
 import { getIronSession } from "iron-session";
-import { sessionOptions,SessionData } from "@/lib/session";
+import { sessionOptions, SessionData } from "@/lib/session";
 import { auth } from "@clerk/nextjs/server";
 interface RecordEventProps {
     name: string;
     params: string;
 }
-const recordEvent = async ({ name,params }: RecordEventProps,userId:string,sessionid:string) => {
+const recordEvent = async ({ name, params }: RecordEventProps, userId: string, sessionid: string) => {
 
-    userId=userId || sessionid;
-    const url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/record-event?name=${encodeURIComponent(name)}&sessionid=${encodeURIComponent(sessionid)}&params=${encodeURIComponent(params)}`;
-   // console.log("record-event:",url)
+    userId = userId || sessionid;
+    const url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/record-event?name=${encodeURIComponent(name)}&sessionid=${encodeURIComponent(sessionid)}&userId=${encodeURIComponent(userId)}&params=${encodeURIComponent(params)}`;
+    // console.log("record-event:",url)
     const fetchResponse = await fetch(url);
     const res = await fetchResponse.json();
     //console.log("RET record-event:",res.success)
     return res.success;
 }
- const workRecordEvent = async (name: string, params: string) => {
+const workRecordEvent = async (name: string, params: string) => {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
     const { userId } = auth() || { userId: "" };
-    const sessionid=session.sessionid;
-    return recordEvent({name,params}, userId||"",sessionid);
+    const sessionid = session.sessionid;
+    return recordEvent({ name, params }, userId || "", sessionid);
 }
-export const actionRecordEvent=(name:string,params:string)=>{
-    return workRecordEvent(name,params);
+export const actionRecordEvent = (name: string, params: string) => {
+    return workRecordEvent(name, params);
 }
 
