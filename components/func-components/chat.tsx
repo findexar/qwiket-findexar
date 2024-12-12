9d343984e39a67dc02154b384c5208d8794aae00
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-12-11 21:46:36 -0500

    Put outage message up

--- components/func-components/chat.tsx
+++ components/func-components/chat.tsx
@@ -878,7 +878,6 @@ const ChatsComponent: React.FC<Props> = ({
                             </div>
                         </div>
                     ))}
-                    <span style={{ color: 'red' }}>WE ARE EXPERIENCING INTERITTENT PROBLEMS DUE TO THE OPENAI OUTAGE.<br />THEY ARE WORKING ON THE RESOLUTION.</span>
                     <div className="flex justify-center items-center h-2 pt-4 text-xs text-gray-500 dark:text-gray-400">
                         {updateMessage || "***"}
                     </div>
