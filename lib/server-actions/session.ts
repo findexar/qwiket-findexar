e85bbeedfe67c168a19f7d8c1adc0f79d3807a0e
Nicholas Dickey <nicholas.dickey@findexar.com>
2025-05-11 08:54:58 -0500

    Add MetaInspector to bots

--- lib/server-actions/session.ts
+++ lib/server-actions/session.ts
@@ -7,7 +7,7 @@ const fetchSession = async (saveSession: boolean = true) => {
     "use server";
    // console.log("===>FETCH SESSION1",sessionOptions)
     let session = await getIronSession<SessionData>(await cookies(), sessionOptions);
-   // console.log("===>FETCH SESSION2",session)
+    console.log("===>FETCH SESSION2",session)
     if (!session.sessionid) {
         var randomstring = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         session.sessionid = randomstring();
